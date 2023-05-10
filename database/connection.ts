06a466a77a21dfc971a184e7780ca78628e09862
adrian-konieczny <adi15090212@wp.pl>
2023-05-10 12:20:25 +0200

    deleted database folder

--- database/connection.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-export const main = async () => {
-  mongoose.connect(
-    "mongodb+srv://admin:UhBnjYwCunPstXes@github-profile-viewer.uaxxp8r.mongodb.net/?retryWrites=true&w=majority"
-  );
-  console.log("Database Connected.");
-};
